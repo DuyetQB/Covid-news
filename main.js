e2808f1847d89a4f72cb37ac67f70c2a7f4fd8c5
Đinh Sỹ Duyệt kênh <duyetsydinh@gmail.com>
2021-03-26 11:40:27 +0700

    update-ui

--- main.js
+++ main.js
@@ -21,14 +21,14 @@ return data ;
 
     <h1 class="main-text" >Thông tin Covid-19 </h1>
 
-<h1> Quốc gia :${data.Countries[186].Country}(${data.Countries[186].CountryCode}) </h1>
+<h1 class="country-name" > Quốc gia :${data.Countries[186].Country}(${data.Countries[186].CountryCode}) </h1>
 <p> Ngày  : ${data.Date}</p>
-<p> Tổng số người mắc : ${data.Countries[186].TotalConfirmed} </p>
+<p> Tổng số người mắc : ${data.Countries[186].TotalConfirmed} Người</p>
 
-<p> Số người chết : ${data.Countries[186].TotalDeaths} </p>
-<p> Ca mắc mới  : ${data.Countries[186].NewConfirmed} </p>
-<p> Số người chết mới : ${data.Countries[186].NewDeaths} </p>
-<p> Số ca chữa khỏi : ${data.Countries[186].TotalRecovered} </p>
+<p> Số người chết : ${data.Countries[186].TotalDeaths}  Người.</p>
+<p> Ca mắc mới  : ${data.Countries[186].NewConfirmed} Ca . </p>
+<p> Số người chết mới : ${data.Countries[186].NewDeaths} Người . </p>
+<p> Số ca chữa khỏi : ${data.Countries[186].TotalRecovered} Ca .</p>
 </div>`
 
 })
